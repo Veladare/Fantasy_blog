7bbb239c6ef96fe1da8c2b1450519fb4bf6f16af
Jean-Luc <jeanlucv@berkeley.edu>
2023-08-10 23:30:27 -0700

    linked handlebars to mainroutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const apiRoutes = require("./api");
+const mainRoutes = require('./mainRoute')
+
 
 router.use("/api", apiRoutes)
-
+router.use('/', mainRoutes)
 
 module.exports = router;
